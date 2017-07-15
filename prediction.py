377ab21000a5876d9acba9e415b0c86b15169495
Celien Nanson <c.nanson@sofort.com>
2017-07-15 16:24:32 +0200

    Adapt prediction to python3

--- prediction.py
+++ prediction.py
@@ -5,14 +5,17 @@ import json
 
 def main():
 	gnb = GNB()
-	with open('train.json', 'rb') as f:
+	with open('train.json', 'r') as f:
 		j = json.load(f)
-	print j.keys()
+	for key, value in j.items():
+		print("Key: ")
+		print(key)
+
 	X = j['states']
 	Y = j['labels']
 	gnb.train(X, Y)
 
-	with open('test.json', 'rb') as f:
+	with open('test.json', 'r') as f:
 		j = json.load(f)
 
 	X = j['states']
@@ -23,7 +26,7 @@ def main():
 		if predicted == label:
 			score += 1
 	fraction_correct = float(score) / len(X)
-	print "You got {} percent correct".format(100 * fraction_correct)
+	print("You got {} percent correct".format(100 * fraction_correct))
 
 if __name__ == "__main__":
 	main()
